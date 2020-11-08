07542a1eb173cffe606e6d43a3089e8c29e0b587
Charlie Liu <liu-za18@mails.tsinghua.edu.cn>
2020-11-09 00:19:45 +0800

    lab3

--- Homework/protocol/protocol.cpp
+++ Homework/protocol/protocol.cpp
@@ -1,7 +1,9 @@
 #include "rip.h"
 #include <stdint.h>
 #include <stdlib.h>
-
+#include<iostream>
+#include <arpa/inet.h>
+using namespace std;
 /*
   在头文件 rip.h 中定义了结构体 `RipEntry` 和 `RipPacket` 。
   你需要从 IPv4 包中解析出 RipPacket 结构体，也要从 RipPacket 结构体构造出对应的
@@ -27,7 +29,44 @@
  */
 bool disassemble(const uint8_t *packet, uint32_t len, RipPacket *output) {
   // TODO:
-  return false;
+  int totallen=(int)packet[2]*256+(int)packet[3];
+  if(totallen>(int)len)
+    return false;
+  int command=(int)packet[28];
+  if(command!=1&&command!=2)
+    return false;
+  int version=(int)packet[29];
+  if(version!=2)
+    return false;
+  int zero=(int)packet[30]*256+packet[31];
+  if(zero!=0)
+    return false;
+  int family=(int)packet[32]*256+packet[33];
+  if(!((command==1&&family==0)||(command==2&&family==2)))
+    return false;
+  int tag=(int)packet[34]*256+packet[35];
+  if(tag!=0)
+    return false;
+  int riplen=((int)len-32)/20;
+  output->numEntries=(uint32_t)riplen;
+  output->command=packet[28];
+  int count=0;
+  for(int i=32;i<(int)len;i=i+20){
+    output->entries[count].addr=htonl((((uint32_t)packet[i+4])<<24)+(((uint32_t)packet[i+5])<<16)+(((uint32_t)packet[i+6])<<8)+(((uint32_t)packet[i+7])));
+    //printf("%x\n",(output->entries[count].addr));
+    //printf("%x\n",((uint8_t*)&output->entries[count].addr)[3]);
+    //printf("%x\n",((uint8_t)output->entries[count].addr));
+    //printf("%x\n",((uint8_t)output->entries[count].addr));
+    output->entries[count].mask=htonl((((uint32_t)packet[i+8])<<24)+(((uint32_t)packet[i+9])<<16)+(((uint32_t)packet[i+10])<<8)+(((uint32_t)packet[i+11])));
+    output->entries[count].nexthop=htonl((((uint32_t)packet[i+12])<<24)+(((uint32_t)packet[i+13])<<16)+(((uint32_t)packet[i+14])<<8)+(((uint32_t)packet[i+15])));
+    //TODO judge nextmask
+    uint32_t metric=(((uint32_t)packet[i+16])<<24)+(((uint32_t)packet[i+17])<<16)+(((uint32_t)packet[i+18])<<8)+(((uint32_t)packet[i+19]));
+    if((int)metric<1||(int)metric>16)
+      return false;
+    output->entries[count].metric=htonl(metric);
+    count++;
+  }
+  return true;
 }
 
 /**
@@ -43,5 +82,38 @@ bool disassemble(const uint8_t *packet, uint32_t len, RipPacket *output) {
  */
 uint32_t assemble(const RipPacket *rip, uint8_t *buffer) {
   // TODO:
-  return 0;
+  buffer[0]=rip->command;
+  buffer[1]=(uint8_t)2;
+  buffer[2]=(uint8_t)0;
+  buffer[3]=(uint8_t)0;
+  int len=4;
+  uint32_t family;
+  if((int)rip->command==1)
+    family=(uint8_t)0;
+  else if((int)rip->command==2)
+    family=(uint8_t)2;
+  for(int i=0;i<rip->numEntries;i++){
+    buffer[4+20*i]=(uint8_t)0;
+    buffer[5+20*i]=family;
+    buffer[6+20*i]=(uint8_t)0;
+    buffer[7+20*i]=(uint8_t)0;
+    buffer[8+20*i]=((uint8_t*)&rip->entries[i].addr)[0];
+    buffer[9+20*i]=((uint8_t*)&rip->entries[i].addr)[1];
+    buffer[10+20*i]=((uint8_t*)&rip->entries[i].addr)[2];
+    buffer[11+20*i]=((uint8_t*)&rip->entries[i].addr)[3];
+    buffer[12+20*i]=((uint8_t*)&rip->entries[i].mask)[0];
+    buffer[13+20*i]=((uint8_t*)&rip->entries[i].mask)[1];
+    buffer[14+20*i]=((uint8_t*)&rip->entries[i].mask)[2];
+    buffer[15+20*i]=((uint8_t*)&rip->entries[i].mask)[3];
+    buffer[16+20*i]=((uint8_t*)&rip->entries[i].nexthop)[0];
+    buffer[17+20*i]=((uint8_t*)&rip->entries[i].nexthop)[1];
+    buffer[18+20*i]=((uint8_t*)&rip->entries[i].nexthop)[2];
+    buffer[19+20*i]=((uint8_t*)&rip->entries[i].nexthop)[3];
+    buffer[20+20*i]=((uint8_t*)&rip->entries[i].metric)[0];
+    buffer[21+20*i]=((uint8_t*)&rip->entries[i].metric)[1];
+    buffer[22+20*i]=((uint8_t*)&rip->entries[i].metric)[2];
+    buffer[23+20*i]=((uint8_t*)&rip->entries[i].metric)[3];
+    len=len+20;
+  }
+  return len;
 }
